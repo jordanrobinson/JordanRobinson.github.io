2e72cc39c6af4fc19998a8aa2ae705b332201b8b
Jordan Robinson <hypahnova@gmail.com>
2015-12-07 22:47:06 +0000

    further minimal design work

--- scripts/site.js
+++ scripts/site.js
@@ -1,91 +1,164 @@
-//Animations
+(function (global) {
+  'use strict';
 
-var fadeIn = function(element, timeout) {
-	setTimeout(function() {
-		element.className = element.className + " loaded";
-	}, timeout);
-};
+  var cheet,
+      sequences = {},
+      keys = {
+        left:37,up:38,right:39,down:40,a:65,b:66
+      },
+      Sequence,
+      NOOP = function NOOP() {},
+      held = {};
 
-var fetchJSONFile = function(path, callback) {
-	var httpRequest = new XMLHttpRequest();
-	httpRequest.onreadystatechange = function() {
-		if (httpRequest.readyState === 4) {
-			if (httpRequest.status === 200 || httpRequest.status === 0) {
-				var data = JSON.parse(httpRequest.responseText);
-				if (callback) {
-					callback(data);
-				}
-			}
-		}
-	};
-	httpRequest.open('GET', path);
-	httpRequest.send(); 
-};
+  Sequence = function Sequence (str, next, fail, done) {
+    var i;
 
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+    this.str = str;
+    this.next = next ? next : NOOP;
+    this.fail = fail ? fail : NOOP;
+    this.done = done ? done : NOOP;
+
+    this.seq = str.split(' ');
+    this.keys = [];
+
+    for (i=0; i<this.seq.length; ++i) {
+      this.keys.push(keys[this.seq[i]]);
     }
-    return array;
-}
 
-function addStat(stat, element, timeout) {
-	var newStat = document.createElement('p');
-	newStat.appendChild(document.createTextNode(stat));
-	newStat.className = newStat.className + "fadein-text";
-	element.appendChild(newStat);
+    this.idx = 0;
+  };
 
-	fadeIn(newStat, timeout);
-}
+  Sequence.prototype.keydown = function keydown (keyCode) {
+    var i = this.idx;
+    if (keyCode !== this.keys[i]) {
+      if (i > 0) {
+        this.reset();
+        this.fail(this.str);
+        cheet.__fail(this.str);
+      }
+      return;
+    }
+
+    this.next(this.str, this.seq[i], i, this.seq);
+    cheet.__next(this.str, this.seq[i], i, this.seq);
+
+    if (++this.idx === this.keys.length) {
+      this.done(this.str);
+      cheet.__done(this.str);
+      this.reset();
+    }
+  };
+
+  Sequence.prototype.reset = function () {
+    this.idx = 0;
+  };
+
+  cheet = function cheet (str, handlers) {
+    var next, fail, done;
+
+    if (typeof handlers === 'function') {
+      done = handlers;
+    } else if (handlers !== null && handlers !== undefined) {
+      next = handlers.next;
+      fail = handlers.fail;
+      done = handlers.done;
+    }
+
+    sequences[str] = new Sequence(str, next, fail, done);
+  };
+
+  cheet.disable = function disable (str) {
+    delete sequences[str];
+  };
+
+  function keydown (e) {
+    var id,
+        k = e ? e.keyCode : event.keyCode;
+
+    if (held[k]) return;
+    held[k] = true;
+
+    for (id in sequences) {
+      sequences[id].keydown(k);
+    }
+  }
+
+  function keyup (e) {
+    var k = e ? e.keyCode : event.keyCode;
+    held[k] = false;
+  }
+
+  function resetHeldKeys (e) {
+    var k;
+    for (k in held) {
+      held[k] = false;
+    }
+  }
+
+  function on (obj, type, fn) {
+    if (obj.addEventListener) {
+      obj.addEventListener(type, fn, false);
+    } else if (obj.attachEvent) {
+      obj['e' + type + fn] = fn;
+      obj[type + fn] = function () {
+        obj['e' + type + fn](window.event);
+      };
+      obj.attachEvent('on' + type, obj[type + fn]);
+    }
+  }
+
+  on(window, 'keydown', keydown);
+  on(window, 'keyup', keyup);
+  on(window, 'blur', resetHeldKeys);
+  on(window, 'focus', resetHeldKeys);
+
+  cheet.__next = NOOP;
+  cheet.next = function next (fn) {
+    cheet.__next = fn === null ? NOOP : fn;
+  };
+
+  cheet.__fail = NOOP;
+  cheet.fail = function fail (fn) {
+    cheet.__fail = fn === null ? NOOP : fn;
+  };
+
+  cheet.__done = NOOP;
+  cheet.done = function done (fn) {
+    cheet.__done = fn === null ? NOOP : fn;
+  };
+
+  cheet.reset = function reset (id) {
+    var seq = sequences[id];
+    if (!(seq instanceof Sequence)) {
+      console.warn('cheet: Unknown sequence: ' + id);
+      return;
+    }
+
+    seq.reset();
+  };
+
+  global.cheet = cheet;
+
+  if (typeof define === 'function' && define.amd) {
+    define([], function () { return cheet; });
+  } else if (typeof module !== 'undefined' && module !== null) {
+    module.exports = cheet;
+  }
+
+})(this);
+
 
 document.onreadystatechange = function () {
 	if (document.readyState == 'complete') {
 
-		var textElements = document.getElementsByClassName('fadein-text');
-		var linkElements = document.getElementsByClassName('links');
-		var timeout = 500;
-
-		for (var i = 0; i < linkElements.length; i++) {
-			fadeIn(linkElements[i], timeout);
-			timeout += 500;
-		}
-
-		for (var j = 0; j < textElements.length; j++) {
-			fadeIn(textElements[j], timeout);
-			timeout += 200;
-		}
-
-		timeout += 3000;
-
-		fetchJSONFile('json/statistics.json', function(data){
-
-			statistics = shuffleArray(data.statistics);
-
-			for (var k = 0; k < 7; k++) {
-				var rightSidebar = document.getElementsByClassName('right-sidebar');
-				for (var l = 0; l < rightSidebar.length; l++) {
-					if (k === 0) {
-						addStat("Site statistics...", rightSidebar[l], timeout);
-					}
-					else if (k === 6) {
-						addStat('There are currently 12 of these statistics, randomly shown', rightSidebar[l], timeout);
-					}
-					else {
-						addStat(statistics[k], rightSidebar[l], timeout);						
-					}
-					timeout += 2000;
-				}
-			}
-
-		});
-
 		cheet('up up down down left right left right b a', function() {
-			var konami = document.getElementsByClassName('heading');
+			var konami = document.getElementsByClassName('title');
 			for (var i = 0; i < konami.length; i++) {
-				konami[0].className = konami[0].className + " konami";
+				konami[i].className = konami[i].className + " konami";
+			}
+			var dark = document.getElementsByClassName('background');
+			for (var j = 0; j < dark.length; j++) {
+				dark[j].className = dark[j].className + " dark";
 			}
 		});
 
